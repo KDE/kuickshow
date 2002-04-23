438325fb569f7cc4b8f505f1c2bafa685a446d43
Marcus Meissner <marcus@jet.franken.de>
2002-04-23 19:32:43 +0000

    Support for *.p?m types, in both file open dialog and MIME Types.
    svn path=/branches/KDE_3_0_BRANCH/kdegraphics/kuickshow/; revision=151508

--- src/kuickdata.cpp
+++ src/kuickdata.cpp
@@ -20,7 +20,7 @@
 
 KuickData::KuickData()
 {
-  fileFilter  = "*.jpeg *.jpg *.gif *.xpm *.ppm *.pgm *.png *.bmp *.psd *.eim *.tiff *.xcf";// *.mng";
+  fileFilter  = "*.jpeg *.jpg *.gif *.xpm *.ppm *.pgm *.pbm *.pnm *.png *.bmp *.psd *.eim *.tiff *.xcf";// *.mng";
   slideDelay       = 3000;
   preloadImage     = true;
 
