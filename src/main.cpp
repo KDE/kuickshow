3aeeabd0510cb008f312a85edfa6cc03056e08db
Carsten Pfeiffer <pfeiffer@kde.org>
2002-07-25 07:22:01 +0000

    2002 we have :)
    svn path=/trunk/kdegraphics/kuickshow/; revision=168636

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ int main(int argc, char **argv)
     KAboutData about(
 	  "kuickshow", I18N_NOOP( "KuickShow" ), KUICKSHOWVERSION,
 	  I18N_NOOP("A fast and versatile image viewer" ),
-	  KAboutData::License_GPL, "(c) 1998-2001, Carsten Pfeiffer",
+	  KAboutData::License_GPL, "(c) 1998-2002, Carsten Pfeiffer",
 	  0 /*text*/, "http://devel-home.kde.org/~pfeiffer/",
 	  "pfeiffer@kde.org" );
 
