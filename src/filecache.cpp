20f05541c098cef7079cb933643d048a3eeb07a8
Carsten Pfeiffer <pfeiffer@kde.org>
2009-04-23 19:52:41 +0000

    picky picky commit filters (add licence)
    svn path=/trunk/extragear/graphics/kuickshow/; revision=958333

--- src/filecache.cpp
+++ src/filecache.cpp
@@ -1,3 +1,20 @@
+/* This file is part of the KDE project
+   Copyright (C) 2003,2009 Carsten Pfeiffer <pfeiffer@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation, version 2.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
 #include <unistd.h>
 
 #include <qstring.h>
--- src/filecache.h
+++ src/filecache.h
@@ -1,12 +1,20 @@
-/****************************************************************************
-** $Id: .emacs,v 1.3 2006/02/20 15:06:53 gis Exp $
-**
-** Created : 2006
-**
-** Copyright (C) 2006 Carsten Pfeiffer <pfeiffer@kde.org>
-**
-****************************************************************************/
+/* This file is part of the KDE project
+   Copyright (C) 2003,2009 Carsten Pfeiffer <pfeiffer@kde.org>
 
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation, version 2.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
 #ifndef FILECACHE_H
 #define FILECACHE_H
 
