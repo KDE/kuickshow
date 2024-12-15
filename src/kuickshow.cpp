711feee54c1c0c1a18d9e21a0414d6ce4e14e3b1
Christian Gerloff <chrgerloff@gmx.net>
2024-12-15 17:23:14 +0100

    chore: replace usage of KStandardGuiItem::Yes
    The element no longer exists in KF6.

--- src/kuickshow.cpp
+++ src/kuickshow.cpp
@@ -170,8 +170,8 @@ KuickShow::KuickShow( const char *objName )
                         "Do you really want to display this 1 image at the same time? This might be quite resource intensive and could overload your computer.<br/>If you choose <interface>%2</interface>, only the first image will be shown.",
                         "Do you really want to display these %1 images at the same time? This might be quite resource intensive and could overload your computer.<br/>If you choose <interface>%2</interface>, only the first image will be shown.", numArgs, KStandardGuiItem::cancel().plainText()),
                 i18n("Display Multiple Images?"),
-                KGuiItem(i18n("Display"), KStandardGuiItem::guiItem(KStandardGuiItem::Yes).icon()),
-                KStandardGuiItem::cancel())!=KMessageBox::Yes)
+                KGuiItem(i18n("Display"), KStandardGuiItem::guiItem(KStandardGuiItem::Ok).icon()),
+                KStandardGuiItem::cancel()) != KMessageBox::PrimaryAction)
         {
             numArgs = 1;
         }
